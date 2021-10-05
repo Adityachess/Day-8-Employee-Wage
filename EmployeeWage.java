b35c18a4ce279be34ff2c6ab7d069a46a4c7e207
Adityachess <adityadone9398@gmail.com>
2021-10-05 13:01:15 +0530

    ADD:Reporting Whether Employee is Present or Absent.

---
+++ EmployeeWage.java
@@ -0,0 +1,29 @@
+package com.bz.employeewage;
+
+public class EmployeeWage {
+
+	public void employeeAttdence() {
+
+		int empCheck = (int) (Math.random() * 10) % 2;
+
+		System.out.println(empCheck);
+
+		if (empCheck == 1) {
+
+			System.out.println("Employee is Present");
+		}
+
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		EmployeeWage object = new EmployeeWage();
+		
+		object.employeeAttdence();
+
+	}
+
+}
